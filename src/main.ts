f7f28ebe32fe4bda618445673c67e028213fd2b4
Finley Ge <m13203533462@163.com>
2024-02-06 21:35:31 +0800

    chore(*): init

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import App from "./App.vue";
+import { createApp } from "vue";
+import "@styles/style.css";
+
+const app = createApp(App);
+app.mount("#app");
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import {ViteAliases} from "vite-aliases";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [vue(), ViteAliases({
+    prefix: "@",
+  })],
+});
